66f736252052354590440f7338c44b63ee3bdbc4
= <=>
2020-06-19 23:40:29 +0100

    Built for heroku and add files image

---
+++ dist/precache-manifest.3f069bcb89f464608b501e85157787c7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9211869b0d55db49d6a",
+    "url": "/css/app.2e7b474e.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "d5b54d67f0300d81dc600b0f6844a105",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37513e81b2acd7b8642a",
+    "url": "/js/about.9efa9a55.js"
+  },
+  {
+    "revision": "d9211869b0d55db49d6a",
+    "url": "/js/app.338825cb.js"
+  },
+  {
+    "revision": "63356ccd149785f0f87f",
+    "url": "/js/chunk-vendors.79b4cfa2.js"
+  },
+  {
+    "revision": "ab14f408c6cc11d1e7c113e38e1fbfa5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
