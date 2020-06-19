1d789ab0e41cc8803addb797b11a2db9ed931beb
= <=>
2020-06-20 00:43:22 +0100

    pruebas de sincronización en varias repos

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    Bienvenido al test
+    <h1>Welcome to test!</h1>
+    <h4>Ready!</h4>
   </div>
 </template>
 
 <script>
-export default {
-  name: "Home"
-};
+  export default {
+    name: "Home",
+  };
 </script>
