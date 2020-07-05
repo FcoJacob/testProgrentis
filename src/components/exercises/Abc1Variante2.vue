d5a67dcf32611c7db1b7d0ee178d09f23040defc
= <=>
2020-07-05 18:33:49 +0100

    intentando hacer recuentos en un contenedor padre de sus hijos

--- src/components/exercises/Abc1Variante2.vue
+++ src/components/exercises/Abc1Variante2.vue
@@ -1,17 +1,33 @@
 <template>
   <v-container>
     <v-row align="center" justify="center">
-      <v-col cols="12" md="5" class="text-left">
+      <v-col
+        cols="12"
+        md="5"
+        :order="$vuetify.breakpoint.mdAndDown ? '2' : '1'"
+        class="text-left"
+      >
         <h2>Datos: {{ dragValue }}</h2>
         <v-card flat width="100%" height="500px">
           <v-img
             class="carry"
             :src="require('@/assets/resourceImg/carry.svg')"
+            contain
           />
           <v-row align="center" justify="center" class="fill-height">
-            <v-col cols="12" md="5" offset-md="2" class="mb-14" id="carryBox">
+            <v-col
+              cols="12"
+              md="5"
+              offset-md="2"
+              class="mb-14"
+              :id="
+                $vuetify.breakpoint.mdAndDown
+                  ? 'carryBox-mobile'
+                  : 'carryBox-desktop'
+              "
+            >
               <v-row
-                class="fill-height"
+                class="d-flex fill-height"
                 id="data1"
                 align="center"
                 justify="center"
@@ -23,30 +39,39 @@
           </v-row>
         </v-card>
       </v-col>
-      <v-col cols="10" md="5">
-        <v-card flat color="transparent" class="mt-8 pa-6" height="500px">
+      <v-col
+        cols="12"
+        md="5"
+        :order="$vuetify.breakpoint.mdAndDown ? '1' : '2'"
+      >
+        <v-card flat color="transparent" class="mt-md-8 pa-md-6 fill-height">
           <v-row align="center" justify="center" class="fill-height">
-            <v-col cols="12" id="appleBox">
+            <v-col cols="12" id="appleBox" class="pa-0">
               <v-row
                 align="center"
                 justify="center"
                 id="data2"
                 :class="
-                  isDragged ? 'borderDropZone fill-height' : 'fill-height'
+                  isDragged
+                    ? 'borderDropZone fill-height mx-auto'
+                    : 'fill-height mx-auto'
                 "
                 @dragover.prevent
                 @drop.prevent="drop()"
               >
                 <v-col
+                  cols="5"
                   v-for="apple in items"
                   :key="apple.id"
                   :id="apple.id"
+                  class="ma-2"
+                  :order="apple.value"
                   :draggable="true"
                   @dragstart="drag(apple.id, apple.value)"
                   @dragover.prevent
                   @drop.stop
                 >
-                  <v-img class="apple ma-4" :src="apple.img" contain />
+                  <v-img class="apple" :src="apple.img" contain />
                 </v-col>
               </v-row>
             </v-col>
@@ -115,16 +140,21 @@ export default {
   },
   methods: {
     appleInCarry() {
-      var carry = document.querySelectorAll('#data2')
-      console.log(carry.length)
+      var carry = document.getElementById('#data2')
+      var appleBox = document.getElementById('#data1')
+      var countApple = appleBox.childNodes.length
+      var countAppleInCarry = carry.childNodes.length
+      console.log(countAppleInCarry + ' ' + countApple)
       if (carry) {
         console.log('dentro')
       } else {
+        this.dragValue = 0
         console.log('fuera')
       }
     },
     drag(id, value) {
       this.dragId = '#' + id
+      this.dragged = true
       this.dragValue = value
       document.addEventListener('dragstart', function(event) {
         /* console.log(event.target.id) */
@@ -133,6 +163,7 @@ export default {
       })
     },
     drop() {
+      this.dragged = true
       document.addEventListener('drop', function(event) {
         // console.log(event)
         if (event.target.id) {
@@ -167,19 +198,29 @@ export default {
   height: 100%;
   z-index: 0;
 }
-#carryBox {
-  background: grey;
+#carryBox-mobile {
+  /* background: grey; */
   position: absolute;
-  top: 25%;
-  left: 25%;
-  width: 100%;
-  height: 200px;
+  top: 37%;
+  left: 28%;
+  width: 55%;
+  height: 20%;
+  z-index: 10;
+  transform: rotate(-7deg);
+}
+#carryBox-desktop {
+  /* background: grey; */
+  position: absolute;
+  top: 30%;
+  left: 20%;
+  width: 55%;
+  height: 30%;
   z-index: 10;
   transform: rotate(-7deg);
 }
 #appleBox {
   text-align: center;
-  background: hotpink;
+  /* background: hotpink; */
   position: relative;
   width: 100%;
   height: 100%;
@@ -188,9 +229,9 @@ export default {
   border: 2px dashed darkslategrey !important;
 }
 .apple {
-  background: indigo;
+  /* background: indigo; */
   position: relative;
-  width: 150px;
+  width: 100%;
   z-index: 1;
 }
 </style>
